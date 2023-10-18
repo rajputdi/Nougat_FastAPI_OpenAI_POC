0d1e0f1ebfb5fd53945f65584af9075d6173a973
rajputdi <rajput.di@northeastern.edu>
2023-10-17 20:46:25 -0400

    Update main.py

--- fastapi_service/main.py
+++ fastapi_service/main.py
@@ -5,8 +5,17 @@ from pdf_extractor import (
     extract_text_nougat,
 )  # Import the necessary functions
 import requests
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 
 @app.post("/extract-text/")
