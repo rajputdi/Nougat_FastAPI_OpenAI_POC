c59c9db601a0333d6c1f1757314e8fef403ffdda
rajputdi <rajput.di@northeastern.edu>
2023-10-17 20:14:58 -0400

    Update api_client.py

--- streamlit_app/api_client.py
+++ streamlit_app/api_client.py
@@ -2,7 +2,7 @@ import requests
 
 
 def extract_text(url, library, nougat_api_address=None):
-    api_url = "http://127.0.0.1:8000/extract-text/"  # Update the address if your FastAPI service is hosted elsewhere
+    api_url = "https://damg7245-asng2-team4-fd9f8dd6d40f.herokuapp.com/"  # Update the address if your FastAPI service is hosted elsewhere
     data = {"url": url, "library": library, "nougat_api_address": nougat_api_address}
     response = requests.post(api_url, json=data)
     response.raise_for_status()  # This will raise an exception for bad responses
