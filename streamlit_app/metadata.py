43118137b4e52c0affcba87bc220a10b2f4086bc
rajputdi <rajput.di@northeastern.edu>
2023-10-19 18:05:50 -0400

    Update metadata.py

--- streamlit_app/metadata.py
+++ streamlit_app/metadata.py
@@ -9,9 +9,13 @@ def get_metadata(text):
     # Splitting text by whitespace to count words
     num_words = len(text.split())
 
+    # Calculating the cost of the query based on the number of tokens
+    cost = (num_tokens / 1000) * 0.0060
+
     return {
         "Model": "text-davinci-002",
         "Encoding Name": "p50k_base",
         "Number of Tokens": num_tokens,
         "Number of Words": num_words,
+        "Rough Cost of Query ($)": cost,
     }
