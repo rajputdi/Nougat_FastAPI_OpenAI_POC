0fd3c9a6ffbf8401a645e18e77e8d0fa4bb3501a
rajputdi <rajput.di@northeastern.edu>
2023-10-19 14:14:07 -0400

    changes to incorporate exception handling

--- streamlit_app/api_client.py
+++ streamlit_app/api_client.py
@@ -1,3 +1,14 @@
+# import requests
+
+
+# def extract_text(url, library, nougat_api_address=None):
+#     api_url = "https://damg7245-asng2-team4-fd9f8dd6d40f.herokuapp.com/extract-text"  # Update the address if your FastAPI service is hosted elsewhere
+#     data = {"url": url, "library": library, "nougat_api_address": nougat_api_address}
+#     response = requests.post(api_url, json=data)
+#     response.raise_for_status()  # This will raise an exception for bad responses
+#     return response.json()
+
+
 import requests
 
 
@@ -5,5 +16,10 @@ def extract_text(url, library, nougat_api_address=None):
     api_url = "https://damg7245-asng2-team4-fd9f8dd6d40f.herokuapp.com/extract-text"  # Update the address if your FastAPI service is hosted elsewhere
     data = {"url": url, "library": library, "nougat_api_address": nougat_api_address}
     response = requests.post(api_url, json=data)
-    response.raise_for_status()  # This will raise an exception for bad responses
-    return response.json()
+    if response.status_code != 200:
+        return (
+            response.json(),
+            response.status_code,
+        )  # Return both data and status code for non-200 responses
+    response.raise_for_status()  # This will raise an exception for bad responses (other than 400)
+    return response.json(), response.status_code  # Return both data and status code
--- streamlit_app/api_ui.py
+++ streamlit_app/api_ui.py
@@ -54,6 +54,37 @@
 # # Ensure the correct import for requests exceptions
 # import requests
 # from requests.exceptions import RequestException
+
+
+# import streamlit as st
+# from api_client import (
+#     extract_text,
+# )  # Ensure this import is correct based on your file structure
+# import requests
+# from requests.exceptions import RequestException
+
+# st.title("PDF Text Extraction App")
+
+# pdf_url = st.text_input("Enter PDF URL:")
+# library_choice = st.selectbox(
+#     "Choose Library:", options=["pypdf", "nougat"]
+# )  # Added "nougat" option
+# nougat_api_address = (
+#     st.text_input("Enter Nougat API Address:") if library_choice == "nougat" else None
+# )
+
+# if st.button("Extract Text"):
+#     if pdf_url:
+#         try:
+#             response_data = extract_text(pdf_url, library_choice, nougat_api_address)
+#             st.write("Extracted Text:")
+#             st.write(response_data.get("text", "No text extracted"))
+#         except RequestException as e:
+#             st.error(f"An error occurred: {str(e)}")
+#     else:
+#         st.error("Please enter a PDF URL")
+
+
 import streamlit as st
 from api_client import (
     extract_text,
@@ -74,9 +105,18 @@ nougat_api_address = (
 if st.button("Extract Text"):
     if pdf_url:
         try:
-            response_data = extract_text(pdf_url, library_choice, nougat_api_address)
-            st.write("Extracted Text:")
-            st.write(response_data.get("text", "No text extracted"))
+            response_data, status_code = extract_text(
+                pdf_url, library_choice, nougat_api_address
+            )  # Assuming extract_text returns both data and status code
+            if status_code == 200:
+                st.write("Extracted Text:")
+                st.write(response_data.get("text", "No text extracted"))
+            elif status_code == 400:
+                st.error(
+                    response_data.get("detail", "An unexpected error occurred.")
+                )  # Display custom error message from API
+            else:
+                st.error(f"An error occurred: {status_code}")
         except RequestException as e:
             st.error(f"An error occurred: {str(e)}")
     else:
