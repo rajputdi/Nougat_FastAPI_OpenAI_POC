58c94de5782e702514bc0beb78f15a16cb17e133
rajputdi <rajput.di@northeastern.edu>
2023-10-19 22:35:04 -0400

    Update summary.py

--- fastapi_service/summary.py
+++ fastapi_service/summary.py
@@ -7,13 +7,13 @@ async def get_summary(text):
     openai_api_key = os.environ["OPENAI_API_KEY"]
     openai.api_key = openai_api_key
 
-    prompt = f"Please summarize the following text so that a teenager can understand what is present in the extract:\n\n{text}"
+    prompt = f"Please summarize within 250 words the following text that is extracted from a form related to regulation or policies from US Security and Exchange Commission website: \n\n{text}"
 
     # Use the OpenAI API to get the summary
     response = openai.Completion.create(
         model="text-davinci-002",
         prompt=prompt,
-        max_tokens=150,
+        max_tokens=300,
     )
 
     return response.choices[0].text.strip()
