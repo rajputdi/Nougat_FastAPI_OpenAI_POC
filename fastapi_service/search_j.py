12f9c0dcec3ee6c670ec951e3debaf6a3044d731
rajputdi <rajput.di@northeastern.edu>
2023-10-19 22:20:09 -0400

    Update search_j.py

--- fastapi_service/search_j.py
+++ fastapi_service/search_j.py
@@ -1,4 +1,3 @@
-from streamlit_app.api_ui import *
 import openai  # for generating embeddings
 import pandas as pd  # for DataFrames to store article sections and embeddings
 import re  # for cutting <ref> links out of Wikipedia articles
